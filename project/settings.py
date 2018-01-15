b6f71d23432d7aba783c104b9ce963edcd721541
Edixon Castillo <rex.cornelius@gmail.com>
2018-01-15 01:28:26 -0400

    ...

--- project/settings.py
+++ project/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 
-from decouple import config
+# from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -24,15 +24,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 # The SECRET_KEY is provided via an environment variable in OpenShift
 #
-# SECRET_KEY = os.getenv(
-#     'DJANGO_SECRET_KEY',
-#     # safe value used for development when DJANGO_SECRET_KEY might not be set
-#     '9e4@&tw46$l##)zrqe3wi--slqm(ruve&se0^%9#6(_w3ui!c0'
-# )
-SECRET_KEY = config('SECRET_KEY') # Uso de Python Decouple...
+SECRET_KEY = os.getenv(
+    'DJANGO_SECRET_KEY',
+    # safe value used for development when DJANGO_SECRET_KEY might not be set
+    '9e4@&tw46$l##)zrq3wi--slqm(ruze&se0^%9#6(_p3ui!c0'
+)
+# SECRET_KEY = config('SECRET_KEY') # Uso de Python Decouple...
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool) # Uso de Python Decouple...
+DEBUG = os.getenv('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
@@ -139,10 +139,10 @@ INTERNAL_IPS = ['127.0.0.1']
 
 ##### Conf. Adfly...
 # Uso de Python Decouple...
-ADFLY_BASE_HOST=config('ADFLY_BASE_HOST')
-ADFLY_SECRET_KEY=config('ADFLY_SECRET_KEY')
-ADFLY_PUBLIC_KEY=config('ADFLY_PUBLIC_KEY')
-ADFLY_USER_ID=config('ADFLY_USER_ID', cast=int)
+ADFLY_BASE_HOST = os.getenv('ADFLY_BASE_HOST')
+ADFLY_SECRET_KEY = os.getenv('ADFLY_SECRET_KEY')
+ADFLY_PUBLIC_KEY = os.getenv('ADFLY_PUBLIC_KEY')
+ADFLY_USER_ID = os.getenv('ADFLY_USER_ID')
 
 
 
